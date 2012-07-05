2c4595b5705f6a06ff985d1600ec53a8e1893f73
Willy <wts200@gmail.com>
2012-07-04 23:07:32 -0500

    Fix2

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -9,10 +9,6 @@ class DefaultController extends Controller
 
     public function indexAction($name)
     {
-        // Ejemplo
-      if (1==1) {
-        echo "prueba";
-      }
 
         return $this->render('OfertaBundle:Default:index.html.twig', array('name' => $name));
     }
