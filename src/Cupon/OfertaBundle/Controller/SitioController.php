75552c2d1dbfe9a1503b63410a072f062b34b7f1
Willy <wts200@gmail.com>
2012-07-05 20:21:34 -0500

    Subiendo cambios

---
+++ src/Cupon/OfertaBundle/Controller/SitioController.php
@@ -0,0 +1,14 @@
+<?php
+namespace Cupon\OfertaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SitioController extends Controller
+{
+
+    public function estaticaAction($pagina)
+    {
+        return $this->render('OfertaBundle:Sitio:'.$pagina.'.html.twig');
+    }
+
+}
